f197d65d452612fe5ad8765c0302d1b91d6c5031
dinhlockt02 <dinhlockt02@gmail.com>
2022-02-14 10:14:41 +0700

    init folder and add more dependencies

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const api = require('./src/api');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api');
 
 app.get('/', (req, res) => {
   res.status(200).json({
