56aef585c31992937128717ee749c4ea2053057f
dinhlockt02 <dinhlockt02@gmail.com>
2022-02-23 06:47:40 +0700

    Fix env var for deploying

--- src/common/sendEmail.js
+++ src/common/sendEmail.js
@@ -1,37 +1,38 @@
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 
-const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI);
-oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN});
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
 async function sendEmail(options) {
-	const accessToken = await oAuth2Client.getAccessToken();
+  const accessToken = await oAuth2Client.getAccessToken();
 
-	let transporter = nodemailer.createTransport({
-		host: 'smtp.gmail.com',
-		port: 465,
-		secure: true,
-		auth: {
-			type: 'OAUTH2',
-			user: process.env.MAIL,
-			clientId: process.env.CLIENT_ID,
-			clientSecret: process.env.CLIENT_SECRET,
-			accessToken: accessToken,
-			refreshToken: process.env.REFRESH_TOKEN
-		},
-	});
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      type: 'OAUTH2',
+      user: process.env.EMAIL,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      accessToken: accessToken,
+      refreshToken: process.env.REFRESH_TOKEN,
+    },
+  });
 
-	const mailOptions = {
-		from: 'hadanhtuan1210@gmail.com',
-		to: options.to,
-		subject: options.subject,
-		html: options.text,
-	};	
+  const mailOptions = {
+    from: 'hadanhtuan1210@gmail.com',
+    to: options.to,
+    subject: options.subject,
+    html: options.text,
+  };
 
-	
-	transporter.sendMail(mailOptions);
-
+  transporter.sendMail(mailOptions);
 }
-
 
 module.exports.sendEmail = sendEmail;
